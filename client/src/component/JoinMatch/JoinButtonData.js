dfff33f00a063235bde8200e4916de10e082cda5
Will Stockman <WillStockman13@gmail.com>
2016-11-17 17:37:04 -0800

    fixed join game bug

--- client/src/component/JoinMatch/JoinButtonData.js
+++ client/src/component/JoinMatch/JoinButtonData.js
@@ -5,7 +5,9 @@ var JoinButtonData = props => {
   var JoinGame = function(game) {
 	  var screenOverlay = document.getElementById( 'screenOverlay' );
 	  var menuContainer = document.getElementById( 'menuContainer' );
+    var hud = document.getElementById( 'HUD' );
 	  screenOverlay.style.display = '';
+    hud.style.display = 'none';
 	  document.addEventListener('keydown', function(e) {
       if(e.keyCode === 192) {
       screenOverlay.style.display = '-webkit-box';
@@ -22,6 +24,22 @@ var JoinButtonData = props => {
       }
 		}
 	   })
+
+    var button = document.getElementById('resume');
+    button.addEventListener('click', function(e) {
+      screenOverlay.style.display = '-webkit-box';
+      screenOverlay.style.display = '-moz-box';
+      screenOverlay.style.display = 'box';
+      if (menuContainer.style.display === '') {
+        menuContainer.style.display = 'none';
+        hud.style.display = '';
+        document.body.requestPointerLock();
+      } else {
+        menuContainer.style.display = '';
+        hud.style.display = 'none';
+      }
+    });
+    document.getElementById('JoinMatchTitle').style.display = 'none';
 	  clientScene.joinGame(this);
   }
 
