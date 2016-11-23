e7e7962ee60cf895aeddabead3c91f9f21c0d78d
Will Stockman <WillStockman13@gmail.com>
2016-11-23 11:37:14 -0800

    Added experience purchasing skin functionality

--- client/src/component/Store/Store.js
+++ client/src/component/Store/Store.js
@@ -9,46 +9,78 @@ class Store extends React.Component {
 	  this.state = {
 	    user: null,
       skins: [
-      {skinPic: '../../../textures/skins/SmileyFaceSkin.jpg', skin: '/textures/skins/SmileyFace.jpg', name: 'Big Smile'},
-      {skinPic: '../../../textures/skins/coolGuy.jpg', skin: '/textures/skins/coolGuy.jpg', name: 'Cool Guy'},
-      {skinPic: '../../../textures/skins/smile.jpg', skin: '/textures/skins/smile.jpg', name: 'Smiley'},
-      {skinPic: '../../../textures/skins/silly.jpg', skin: '/textures/skins/silly.jpg', name: 'Silly'},
-      {skinPic: '../../../textures/skins/wink.jpg', skin: '/textures/skins/wink.jpg', name: 'Wink'},
-      {skinPic: '../../../textures/skins/sidewaysSmile.jpg', skin: '/textures/skins/sidewaysSmile.jpg', name: 'Sideways Smile'},
-      {skinPic: '../../../textures/skins/twirlyStache.jpg', skin: '/textures/skins/twirlyStache.jpg', name: 'Twirly Stache'},
-      {skinPic: '../../../textures/skins/googlyStache.jpg', skin: '/textures/skins/googlyStache.jpg', name: 'Googly Stache'},
-      {skinPic: '../../../textures/skins/gangstaStache.jpg', skin: '/textures/skins/gangstaStache.jpg', name: 'Gangsta Stache'},
-      {skinPic: '../../../textures/skins/heisenberg.jpg', skin: '/textures/skins/heisenberg.jpg', name: 'Heisenberg'},
-      {skinPic: '../../../textures/skins/heisenbergHardcorePreview.jpg', skin: '/textures/skins/heisenbergHardcore.jpg', name: 'Hardcore Heisenberg'},
-      {skinPic: '../../../textures/skins/glassesStache.jpg', skin: '/textures/skins/glassesStache.jpg', name: 'Glasses N Stache'},
-      {skinPic: '../../../textures/skins/SoccerBallSkin.jpg', skin: '/textures/skins/soccer2.jpg', name: 'Socer Ball'},
-      {skinPic: '../../../textures/skins/superManSkin.jpg', skin: '/textures/skins/superman.jpg', name: 'Superman'},
-      {skinPic: '../../../textures/skins/batmanSkin.jpg', skin: '/textures/skins/Batman.jpg', name: 'Batman'},
-      {skinPic: '../../../textures/skins/8BallSkin.jpg', skin: '/textures/skins/8ball.jpg', name: '8 Ball'},
-      {skinPic: '../../../textures/skins/CaptainAmericaSkin.jpg', skin: '/textures/skins/CaptainAmerica.jpg', name: 'Captain America'}
-      ]
+      {skinPic: '../../../textures/skins/SmileyFaceSkin.jpg', skin: '/textures/skins/SmileyFace.jpg', name: 'Big Smile', price: 500},
+      {skinPic: '../../../textures/skins/coolGuy.jpg', skin: '/textures/skins/coolGuy.jpg', name: 'Cool Guy', price: 200},
+      {skinPic: '../../../textures/skins/smile.jpg', skin: '/textures/skins/smile.jpg', name: 'Smiley', price: 100},
+      {skinPic: '../../../textures/skins/silly.jpg', skin: '/textures/skins/silly.jpg', name: 'Silly', price: 50},
+      {skinPic: '../../../textures/skins/wink.jpg', skin: '/textures/skins/wink.jpg', name: 'Wink', price: 100},
+      {skinPic: '../../../textures/skins/sidewaysSmile.jpg', skin: '/textures/skins/sidewaysSmile.jpg', name: 'Sideways Smile', price: 100},
+      {skinPic: '../../../textures/skins/twirlyStache.jpg', skin: '/textures/skins/twirlyStache.jpg', name: 'Twirly Stache', price: 200},
+      {skinPic: '../../../textures/skins/googlyStache.jpg', skin: '/textures/skins/googlyStache.jpg', name: 'Googly Stache', price: 300},
+      {skinPic: '../../../textures/skins/gangstaStache.jpg', skin: '/textures/skins/gangstaStache.jpg', name: 'Gangsta Stache', price: 400},
+      {skinPic: '../../../textures/skins/heisenberg.jpg', skin: '/textures/skins/heisenberg.jpg', name: 'Heisenberg', price: 600},
+      {skinPic: '../../../textures/skins/heisenbergHardcorePreview.jpg', skin: '/textures/skins/heisenbergHardcore.jpg', name: 'Hardcore Heisenberg', price: 500},
+      {skinPic: '../../../textures/skins/glassesStache.jpg', skin: '/textures/skins/glassesStache.jpg', name: 'Glasses N Stache', price: 500},
+      {skinPic: '../../../textures/skins/SoccerBallSkin.jpg', skin: '/textures/skins/soccer2.jpg', name: 'Socer Ball', price: 50},
+      {skinPic: '../../../textures/skins/superManSkin.jpg', skin: '/textures/skins/superman.jpg', name: 'Superman', price: 400},
+      {skinPic: '../../../textures/skins/batmanSkin.jpg', skin: '/textures/skins/Batman.jpg', name: 'Batman', price: 400},
+      {skinPic: '../../../textures/skins/8BallSkin.jpg', skin: '/textures/skins/8ball.jpg', name: '8 Ball', price: 250},
+      {skinPic: '../../../textures/skins/CaptainAmericaSkin.jpg', skin: '/textures/skins/CaptainAmerica.jpg', name: 'Captain America', price: 400}
+      ],
+      experience: 0,
+      noFunds: false
 	  };
 	  this.backToHome = this.backToHome.bind(this);
+    this.backToStore = this.backToStore.bind(this);
   }
 
   backToHome() {
     browserHistory.push('/Home');
   }
 
+  buyExperience() {
+    console.log('hello')
+  }
+
+  backToStore() {
+    this.state.noFunds = false;
+    browserHistory.push('Store')
+  }
+
   render() {
-    return (
-      <div id='Store'>
-        <div id='CreateMatchBackground' >
-          <div>
-            <h1 id='Title'>Smash Ball Brawl Store</h1>
-          </div>
-          <button id='HOMEButton' className='btn btn-primary' onClick={this.backToHome}>HOME</button>
-          <div id='Skins'>
-            {this.state.skins.map((skins) => <StoreData skins={skins} />)}
+    if(this.state.noFunds === true) {
+      return (
+        <div id='Store'>
+          <div id='CreateMatchBackground' >
+            <div>
+              <h1 id='Title'>Smash Ball Brawl Store</h1>
+            </div>
+            <button id='HOMEButton' className='btn btn-primary' onClick={this.backToHome}>HOME</button>
+            <button id='storeButton' className='btn btn-primary' onClick={this.backToStore}>STORE</button>
+            <div id='buyButton'>
+              <button className='btn btn-success' onClick={this.buyExperience}>Buy Experience</button>
+            </div>
+            <div> You have <span> {userProfile.experience}</span> experience</div>
+            <div>You dont have enough experience to buy the item. To gain experience play more games or purchase them above.</div>
           </div>
         </div>
-      </div>
-    );
+      )
+    } else {
+      return (
+        <div id='Store'>
+          <div id='CreateMatchBackground' >
+            <div>
+              <h1 id='Title'>Smash Ball Brawl Store</h1>
+            </div>
+            <button id='HOMEButton' className='btn btn-primary' onClick={this.backToHome}>HOME</button>
+            <div> You have <span> {userProfile.experience}</span> experience</div>
+            <div id='Skins'>
+              {this.state.skins.map((skins) => <StoreData key={skins.skin} skins={skins} state={this.state}/>)}
+            </div>
+          </div>
+        </div>
+      );   
+    }
 
   }
 }
--- client/src/component/Store/StoreData.js
+++ client/src/component/Store/StoreData.js
@@ -4,8 +4,15 @@ import userProfile from '../userProfile.js';
 
 var StoreData = props => {
 	var addSkin = function() {
-    userProfile.ChosenSkin = props.skins.skin;
-    browserHistory.push('Home')
+    if(userProfile.experience >= props.skins.price) {
+      userProfile.Skins.push(props.skins)
+      userProfile.ChosenSkin = props.skins.skin; 
+      userProfile.experience = userProfile.experience - props.skins.price; 
+      browserHistory.push('Store')
+    } else {
+      props.state.noFunds = true;
+      browserHistory.push('Store')
+    }
   }
 
   return (
@@ -14,8 +21,9 @@ var StoreData = props => {
   	    <img id='SkinPic' src={props.skins.skinPic} />
   	  </div>
   	  <div id='SkinName'>{props.skins.name}</div>
+      <div id='SkinCost'>Cost: <span>{props.skins.price}</span> xp</div>
   	  <div>
-  	    <button id='SkinButton' className='btn btn-warning' onClick={addSkin.bind(props.skin)}>Add Skin</button>
+  	    <button id='SkinButton' className='btn btn-warning' onClick={addSkin}>Buy Skin</button>
   	  </div>
   	</div>
   )
