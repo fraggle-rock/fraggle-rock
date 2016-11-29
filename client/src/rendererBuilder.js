426e4c6118b2cba4a8e8359df9ad9733bac942a4
Eric Eakin <eseakin@gmail.com>
2016-11-28 20:08:23 -0800

    3 graphics settings

--- client/src/rendererBuilder.js
+++ client/src/rendererBuilder.js
@@ -6,17 +6,13 @@ module.exports = {
   const renderer = new THREE.WebGLRenderer({ antialias: true });
   renderer.setSize(window.innerWidth - 5, window.innerHeight - 5);
 
-  if (userProfile.graphics === 3) {
+  if (userProfile.graphics === 2) {
 	  renderer.shadowMap.enabled = true;
 		renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
-  } else if (userProfile.graphics === 2) {
-  	renderer.shadowMap.enabled = true;
-  	renderer.shadowMap.type = THREE.PCFShadowMap;
-
 	} else if (userProfile.graphics === 1) {
 		renderer.shadowMap.enabled = true;
-		renderer.shadowMap.type = THREE.BasicShadowMap ;
+		renderer.shadowMap.type = THREE.BasicShadowMap;
 
   } else {
   	renderer.shadowMap.enabled = false;
