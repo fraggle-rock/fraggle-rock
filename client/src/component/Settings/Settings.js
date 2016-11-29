b105a136f6fa5645fae120b63edd59b004d928e1
Eric Eakin <eseakin@gmail.com>
2016-11-29 12:02:19 -0800

    Removed bootstrap. Buttons are now in css. Changed settings menu and user profile to match. Added Guest as default user.

--- client/src/component/Settings/Settings.js
+++ client/src/component/Settings/Settings.js
@@ -15,9 +15,14 @@ class Settings extends React.Component {
     browserHistory.push('/Home');
   }
 
-  click(btn) {
+  clickGfx(btn) {
     userProfile.graphics = btn;
-    browserHistory.push('Home')
+    browserHistory.push('Home');
+  }
+
+  clickSound(btn) {
+    userProfile.sound = btn;
+    browserHistory.push('Home');
   }
 
   render() {
@@ -28,11 +33,17 @@ class Settings extends React.Component {
               <h1 id='Title'>Settings</h1>
             </div>
             <button id='HOMEButton' className='btn btn-primary' onClick={this.backToHome}>HOME</button>
-            <h3>Choose Graphics Quality</h3>
-            <div id='SettingsButtons'>
-              <button className={'btn btn-danger gfxBtn ' + (userProfile.graphics === 0 ? 'gfxBtnActive' : '')} onClick={() => this.click(0)}>Low</button>
-              <button className={'btn btn-warning gfxBtn ' + (userProfile.graphics === 1 ? 'gfxBtnActive' : '')} onClick={() => this.click(1)}>Medium</button>
-              <button className={'btn btn-success gfxBtn ' + (userProfile.graphics === 2 ? 'gfxBtnActive' : '')} onClick={() => this.click(2)}>High</button>
+            <div className='SettingsButtons'>
+              <h3>Graphics Quality</h3>
+              <button className={'btn gfxBtn ' + (userProfile.graphics === 0 ? 'btn-success' : '')} onClick={() => this.clickGfx(0)}>Low</button>
+              <button className={'btn gfxBtn ' + (userProfile.graphics === 1 ? 'btn-success' : '')} onClick={() => this.clickGfx(1)}>Medium</button>
+              <button className={'btn gfxBtn ' + (userProfile.graphics === 2 ? 'btn-success' : '')} onClick={() => this.clickGfx(2)}>High</button>
+            </div>
+
+            <div className='SettingsButtons'>
+              <h3>Sound</h3>
+              <button className={'btn gfxBtn ' + (userProfile.sound === 1 ? 'btn-success' : '')} onClick={() => this.clickSound(1)}>On</button>
+              <button className={'btn gfxBtn ' + (userProfile.sound === 0 ? 'btn-success' : '')} onClick={() => this.clickSound(0)}>Off</button>
             </div>
           </div>
         </div>
