b89b94d89c10b6c321f8080c7f4826fd96951913
Eric Eakin <eseakin@gmail.com>
2016-11-29 16:32:52 -0800

    added requestjs to node modules. Still working on unifying menu styling

--- client/src/component/CreateMatch/CreateMatch.js
+++ client/src/component/CreateMatch/CreateMatch.js
@@ -14,7 +14,7 @@ class CreateMatch extends React.Component {
 	    user: null,
       maps: userProfile.maps,
       mapChoice: userProfile.map,
-      mapPreviewPath: userProfile.maps[userProfile.map].thumb       
+      mapPreviewPath: userProfile.maps[userProfile.map].thumb
 	  };
 	  this.backToHome = this.backToHome.bind(this);
   }
@@ -110,10 +110,11 @@ class CreateMatch extends React.Component {
             <Profile />
           </div>
           <div id='CreateMatchBackground'>
-            <button id='SelectSkinButtonCreate' className='btn btn-warning' onClick={this.selectSkin}>Select Skin</button>
-            <button id='HomeButtonCreate' className='btn btn-primary' onClick={this.backToHome}>HOME</button>
-            
-            <h1 id='CreateMatchTitle'>Create Match</h1>
+            <div className='buttonBox'>
+              <button className='btn btn-primary homeBtn' onClick={this.backToHome}>HOME</button>
+              <h1>Create Match</h1>
+              <button className='btn btn-warning selectSkinBtn' onClick={this.selectSkin}>Select Skin</button>
+            </div>
 
             <div id='ChooseMap' onClick={this.ChooseMap}>Choose Map</div>
 
