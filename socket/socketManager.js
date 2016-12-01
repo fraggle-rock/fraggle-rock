5504523c0b8cdc0a1bc584e8cf92f42ebbae6d33
NickLathen <nick.lathen@gmail.com>
2016-12-01 12:02:55 -0800

    added timeout for physics servers if no poll for 8 seconds

--- socket/socketManager.js
+++ socket/socketManager.js
@@ -1,3 +1,4 @@
+'use strict';
 const httpPort = 4444;
 const http = require('http');
 const physicsServers = {};
@@ -6,6 +7,18 @@ const server = http.createServer((req, res) => {
   if (req.url === '/register') {
     const serverUrl = req.connection.remoteAddress;
     console.log('registering new physics server at ' + serverUrl);
+    
+    physicsServers[serverUrl] = {status: 'empty'};
+    const server = physicsServers[serverUrl];
+    server.lastUpdate = Date.now();
+    server.timeout = setInterval(function() {
+      if (Date.now() - server.lastUpdate > 8000) {
+        console.log(`Server at ${serverUrl} timed out.`);
+        clearInterval(server.timeout);
+        delete physicsServers[serverUrl];
+      }
+    }, 10 * 1000);
+
     res.statusCode = 200;
     res.end();
   }
@@ -13,6 +26,9 @@ const server = http.createServer((req, res) => {
     console.log('serving livegames')
     res.statusCode = 200;
     res.end('hi from socketmanger');
+  }
+  if (req.url === 'statusPoll') {
+
   }
 });
 server.listen(httpPort);
