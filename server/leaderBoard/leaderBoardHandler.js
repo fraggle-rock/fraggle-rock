8edfb7acc1539c0ead6dc7faf0d4dd283aa8a1ea
Will Stockman <WillStockman13@gmail.com>
2016-12-02 12:11:38 -0800

    Leaderboard is now functional

--- server/leaderBoard/leaderBoardHandler.js
+++ server/leaderBoard/leaderBoardHandler.js
@@ -13,24 +13,29 @@ router.route('/leaderBoard')
     const sortedScores = _.sortBy(Scores, 'score');
     const userIDs = _.pluck(sortedScores, 'user_id');
     const gameIDS = _.pluck(sortedScores, 'game_id');
-    const resultArray = [];
-    UserController.searchUserByids(userIDs)
-    .then((users) => {
-      GameController.searchUserByids(gameIDS)
-      .then((games) => {
-        sortedScores.forEach((sortedScore, index) => {
-          const obj = { userid: users[index].id,
-            username: users[index].username,
-            facebookid: users[index].facebookid,
-            email: users[index].email,
-            score: sortedScore.score,
-          };
-          resultArray.push(obj);
-        });
-        res.status(200).send(resultArray);
+    let resultArray = [];
+    const cumulative = {};
+    sortedScores.forEach((sortedScore, index) => {
+      if(cumulative[sortedScore.user_id] !== undefined) {
+        cumulative[sortedScore.user_id] += sortedScore.score;
+      } else {
+        cumulative[sortedScore.user_id] = sortedScore.score;
+      }
+    })
+
+      UserController.getAllUsers()
+      .then((users) => {
+        for (var key in cumulative) {
+          const user = _.findWhere(users, {"id": parseInt(key)});
+           resultArray.push( { username: user.username, score: cumulative[key] });
+        }
+        
+        resultArray = resultArray.reverse();
+        resultArray =  resultArray.slice(0, 10)
+        res.status(200).send(_.sortBy(resultArray, 'score'));
       });
-    });
   });
 });
+
 
 module.exports = router;
